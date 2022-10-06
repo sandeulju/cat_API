8bf8f44d025fff0cc2a93460e4d3350463f803c1
DESKTOP-JAF34MA\user <tksemf1706@bizwizsystem.co.kr>
2022-10-06 17:51:07 +0900

    fix : onclick 함수 넣을때 실행시켜 넣으면 안됨 - 오류 수정

--- src/component/container/other1/components/TodoCreate.jsx
+++ src/component/container/other1/components/TodoCreate.jsx
@@ -1,5 +1,9 @@
 import { useState, useEffect } from "react";
 
+// https://react.vlpt.us/basic/10-useRef.html
+
+const addList = () => {};
+
 const TodoCreate = () => {
   return (
     <div className="todo-input-wrap">
@@ -8,7 +12,13 @@ const TodoCreate = () => {
         type="text"
         placeholder="오늘의 할일을 입력해주세요!"
       />
-      <input className="todo-submit" type="submit" />
+      <input
+        className="todo-submit"
+        type="button"
+        value={"제출"}
+        onClick={addList}
+        // onClick에 함수 넣을 때 실행시키면 안됨!!!!!!
+      />
     </div>
   );
 };
